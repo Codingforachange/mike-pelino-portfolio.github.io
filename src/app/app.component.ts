2418875d6b9d04c1557dbab93c717170422f8745
mpelin2 <mpelin2@wgu.edu>
2026-03-05 08:46:40 -0500

    Updated Navigation html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { HeroComponent } from "./components/hero/hero.component";
 import { ExperienceComponent } from "./components/experience/experience.component";
 import { SkillsComponent } from "./components/skills/skills.component";
@@ -8,7 +8,7 @@ import { ProjectsComponent } from "./components/projects/projects.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroComponent, ExperienceComponent, SkillsComponent, ProjectsComponent],
+  imports: [RouterOutlet, HeroComponent, ExperienceComponent, SkillsComponent, ProjectsComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
